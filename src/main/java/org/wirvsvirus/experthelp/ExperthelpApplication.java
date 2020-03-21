05897a81dc77318bb6b4f41e1294cf74a3ddbe3d
markus.muellenborn <markus.muellenborn@meinestadt.de>
2020-03-21 12:21:04 +0100

    first project structure

---
+++ src/main/java/org/wirvsvirus/experthelp/ExperthelpApplication.java
@@ -0,0 +1,13 @@
+package org.wirvsvirus.experthelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperthelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExperthelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wirvsvirus/experthelp/ExperthelpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wirvsvirus.experthelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperthelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
